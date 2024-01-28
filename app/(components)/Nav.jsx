6d04ab894b36d0034c4d25262b08c9bb0016bf15
Gaurang Ambasana <gpambasana@gmail.com>
2024-01-28 17:11:48 +0530

    added navbar

---
+++ app/(components)/Nav.jsx
@@ -0,0 +1,12 @@
+const Nav = () => {
+  return (
+    <nav>
+      <div></div>
+      <div>
+        <p className="text-default-text">someone@email.com</p>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
