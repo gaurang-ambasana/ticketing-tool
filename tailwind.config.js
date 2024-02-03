5e421be2af8e02d6dc682aa7e9a1f1c37eea2bc5
Gaurang Ambasana <gpambasana@gmail.com>
2024-02-03 16:15:18 +0530

    added more rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,28 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-    colors: {
-      nav: "#18222f",
-      page: "#2b3441",
-      card: "#47566a",
-      "card-hover": "#4f5e74",
-      "default-text": "#f1f3f5",
-      "blue-accent": "#0084d4",
-      "blue-accent-hover": "#009fff",
+export const content = [
+  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./app/**/*.{js,ts,jsx,tsx,mdx}",
+];
+export const theme = {
+  extend: {
+    backgroundImage: {
+      "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      "gradient-conic":
+        "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
     },
   },
-  plugins: [],
+  colors: {
+    nav: "#18222f",
+    page: "#2b3441",
+    card: "#47566a",
+    "card-hover": "#4f5e74",
+    "default-text": "#f1f3f5",
+    "blue-accent": "#0084d4",
+    "blue-accent-hover": "#009fff",
+    delete: "#EF5350",
+    "delete-hover": "#EF9A9A",
+    fire: "yellow",
+  },
 };
+export const plugins = [];
