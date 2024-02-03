d9ac186bbabb8640db7feddad3048c6ad43d6801
Gaurang Ambasana <gpambasana@gmail.com>
2024-02-03 19:28:11 +0530

    added GET and PUT

--- app/api/tickets/[id]/route.js
+++ app/api/tickets/[id]/route.js
@@ -1,10 +1,36 @@
 import Ticket from "@/app/(models)/Ticket";
 import { NextResponse } from "next/server";
 
-export async function DELETE(req, { params: { id } }) {
+export async function DELETE(_, { params: { id } }) {
   try {
     await Ticket.findByIdAndDelete(id);
     return NextResponse.json({ msg: "Ticket Deleted" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ msg: "Error", error }, { status: 500 });
+  }
+}
+
+export async function GET(_, { params: { id } }) {
+  try {
+    const ticketData = await Ticket.findById(id);
+    return NextResponse.json({ ticketData }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ msg: "Error", error }, { status: 500 });
+  }
+}
+
+export async function PUT(req, { params: { id } }) {
+  try {
+    const body = await req.json();
+    const ticketData = body.formData;
+    const updatedTicketData = await Ticket.findByIdAndUpdate(id, {
+      ...ticketData,
+    });
+
+    return NextResponse.json(
+      { msg: "Ticket Updated!!!", updatedTicketData },
+      { status: 200 }
+    );
   } catch (error) {
     return NextResponse.json({ msg: "Error", error }, { status: 500 });
   }
