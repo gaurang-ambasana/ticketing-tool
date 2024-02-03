670a59561098fa9a2a1e810211316b56bdb1cef0
Gaurang Ambasana <gpambasana@gmail.com>
2024-02-03 19:28:36 +0530

    ticket card clickable

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -3,6 +3,7 @@ import DeleteBlock from "./DeleteBlock";
 import PriorityDisplay from "./PriorityDisplay";
 import ProgressDisplay from "./ProgressDisplay";
 import StatusDisplay from "./StatusDisplay";
+import Link from "next/link";
 
 const TicketCard = ({
   _id,
@@ -21,24 +22,26 @@ const TicketCard = ({
         <DeleteBlock id={_id} name={title} />
       </div>
     </div>
-    <h4>{title}</h4>
-    <hr className="h-px border-0 bg-page mb-2" />
-    <p className="whitespace-pre-wrap">{description}</p>
-    <div className="flex-grow"></div>
-    <div className="flex mt-2">
-      <div className="flex flex-col">
-        <p className="text-xs my-1">
-          Created At: {new Date(createdAt).toString().split("(")[0].trim()}
-        </p>
-        <p className="text-xs my-1">
-          Last Update: {new Date(updatedAt).toString().split("(")[0].trim()}
-        </p>
-        <ProgressDisplay progress={progress} />
+    <Link href={`/TicketPage/${_id}`}>
+      <h4>{title}</h4>
+      <hr className="h-px border-0 bg-page mb-2" />
+      <p className="whitespace-pre-wrap">{description}</p>
+      <div className="flex-grow"></div>
+      <div className="flex mt-2">
+        <div className="flex flex-col">
+          <p className="text-xs my-1">
+            Created At: {new Date(createdAt).toString().split("(")[0].trim()}
+          </p>
+          <p className="text-xs my-1">
+            Last Update: {new Date(updatedAt).toString().split("(")[0].trim()}
+          </p>
+          <ProgressDisplay progress={progress} />
+        </div>
+        <div className="ml-auto flex items-end">
+          <StatusDisplay status={status} />
+        </div>
       </div>
-      <div className="ml-auto flex items-end">
-        <StatusDisplay status={status} />
-      </div>
-    </div>
+    </Link>
   </div>
 );
 
