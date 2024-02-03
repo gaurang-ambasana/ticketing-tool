eb83b6230902c0a2c46cdc1399ee8db75fabc46d
Gaurang Ambasana <gpambasana@gmail.com>
2024-02-03 16:39:45 +0530

    new component

---
+++ app/(components)/StatusDisplay.jsx
@@ -0,0 +1,9 @@
+const StatusDisplay = () => {
+  return (
+    <span className="inline-block rounded-full px-2 text-xs font-semibold text-status-text bg-status-bg">
+      done
+    </span>
+  );
+};
+
+export default StatusDisplay;
